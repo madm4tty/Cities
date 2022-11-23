843647eab99dfc14463f15cd0e79f636fcfdd047
matt <matt.follows@gmail.com>
2022-11-23 08:26:27 +0000

    City selection routine working

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
         print(t_city_df)
         msg = f"There are: {t_city_df_len} results for: {t_city_input}."
         print(msg)
-        input_country = input("Which country is the city in? (use country code):")
+        input_country = input("Which country is the city in? (use country code eg:GB):")
         input_country = input_country.upper()
         print(f"input_country is: {input_country}")
 
@@ -79,57 +79,24 @@ def main():
         t_result_city_len = (len(t_result_city.index))
         print(f"t_result_city_len is: {t_result_city_len}")
         if t_result_city_len > 1:
-            #t_result_city
-            
-            
             print(f"Multiple cities found for {input_country} (see below), defaulting to city with largest population")
             print(t_result_city)
-            
-            #recent_date = df['date'].max()
-            #result_city_max = t_result_city['Population'].max()
-            
             test = t_result_city['Population'].max()
-            print (f"t_result_city: {test}")
-            
-            # top n rows ordered by multiple columns
-           # gapminder_2007.nlargest(3,['lifeExp','gdpPercap'])
-            #gapminder_2007.nlargest(3,['lifeExp','gdpPercap'])
-            result_city_max = t_result_city.nlargest(1,['Population'])
-            
-            #for row in t_result_city.index:
-                #print(f'Max element of row {row} is:', max(t_result_city.iloc[row]))
-             #   print(f'Max element of row {row} is:', max(t_result_city.iloc[row]))
-            
-            
-            #result_city_max = t_result_city.loc[t_result_city['Population'].max(), 'id']
-            #result_city_max = df.loc[df.groupby('YearReleased')['count'].idxmax()]
-           
-            # result_city_max = t_result_city.loc[t_result_city['Population'].idxmax()]
-            #result_city_id = result_city_max.iloc[0]['id']
-            
-            #df2=df.loc[df['Fee'] == 30000, 'Courses']
+            print(f"t_result_city: {test}")
 
-            #df2=df.loc[df['Fee'] == 30000, 'Courses']
+            # Sort results by largest population and return first row
+            result_city_max = t_result_city.nlargest(1, ['Population'])
+            result_city_id = result_city_max.iloc[0]['id']
 
-            
             print(f"result_city_max: {result_city_max}")
-            
-            #result_city_id = result_city_max.iloc[0]['id']
-            #result_city_id = ['Population'].max()
-
-
         else:
             # Take row id value for lookup
             result_city_id = t_result_city.iloc[0]['id']
-            
-
-
-        #print("result_city_id: ", result_city_id)
-
-
+        print("result_city_id: ", result_city_id)
     else:
         # Print results
-        print("t_city_df_len is:", t_city_df_len)
+        result_city_id = t_city_df.iloc[0]['id']
+        print("result_city_id: ", result_city_id)
         print(t_city_df)
 
 
