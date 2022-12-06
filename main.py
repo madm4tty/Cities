2ea94cce7b652d25565f8d93bed994876b6c7155
Matt <madm4tty@users.noreply.github.com>
2022-12-06 15:20:06 +0000

    added country with city

--- main.py
+++ main.py
@@ -198,20 +198,25 @@ def main():
             result_city_max = t_result_city.nlargest(1, ['Population'])
             print(f"result_city_max: {result_city_max}")
             result_city_id = result_city_max.iloc[0]['id']
+            city_t_country = result_city_max.iloc[0]['country']
             print(f"result_city_id: {result_city_id}")
         else:
             # Take row id value for lookup
             pop_t = t_result_city.iloc[0]['Population']
             result_city_id = t_result_city.iloc[0]['id']
+            city_t_country = t_result_city.iloc[0]['country']
+            
     else:
         # Print results
         result_city_id = t_city_df.iloc[0]['id']
         pop_t = t_city_df.iloc[0]['Population']
+        city_t_country = t_city_df.iloc[0]['country']
 
     # Get target city's name
     city_t_name = t_city_df.iloc[0]['city']
 
-    print(f"Target city is: {city_t_name} with a population of: {pop_t}")
+
+    print(f"Target city is: {city_t_name}, {city_t_country} with a population of: {pop_t}")
     
     # Now run the search passing in the variables
     n_city_id, n_city_name, n_city_country, n_city_pop, n_city_dist = \
